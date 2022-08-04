4b402a4264844691d5e2c4090de1a68b5d29930f
ShobhitMysoft <shobhit.mysoft@gmail.com>
2022-08-04 18:27:58 +0530

    Connection request using new way from google dev page

--- app/src/main/java/com/shobhitsagar/bluetoothtest/MainActivity.kt
+++ app/src/main/java/com/shobhitsagar/bluetoothtest/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.shobhitsagar.bluetoothtest
 
 import android.Manifest
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
-import android.bluetooth.BluetoothSocket
+import android.annotation.SuppressLint
+import android.bluetooth.*
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -26,7 +25,7 @@ class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
     private var bluetoothAdapter: BluetoothAdapter? = null
     private var btSocket: BluetoothSocket? = null
-    private val mUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
+    private val MY_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +38,8 @@ class MainActivity : AppCompatActivity() {
         val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
         bluetoothAdapter = bluetoothManager.adapter
 
-        connectToBtDevice()
+        /*connectToBtDevice()*/
+        bluetoothAdapter?.let { ConnectThread(it.getRemoteDevice("94:B5:55:2D:29:32")).run() }
 
         button.setOnClickListener {
             Log.d(TAG, "onCreate: ${editText.text}")
@@ -47,7 +47,85 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun connectToBtDevice() {
+    @SuppressLint("MissingPermission")
+    private inner class ConnectThread(device: BluetoothDevice) : Thread() {
+
+        private val mmSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            device.createRfcommSocketToServiceRecord(MY_UUID)
+        }
+
+        override fun run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            bluetoothAdapter?.cancelDiscovery()
+
+            mmSocket?.let { socket ->
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                socket.connect()
+
+                // The connection attempt succeeded. Perform work associated with
+                // the connection in a separate thread.
+//                manageMyConnectedSocket(socket)
+
+                if (socket.isConnected) {
+                    btSocket = socket
+                    Toast.makeText(applicationContext, "Socket Created", Toast.LENGTH_SHORT).show()
+                }
+                else Toast.makeText(applicationContext, "Socket Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmSocket?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the client socket", e)
+            }
+        }
+    }
+
+    /*private inner class AcceptThread : Thread() {
+
+        private val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            bluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord("NAME", MY_UUID)
+        }
+
+        override fun run() {
+            // Keep listening until exception occurs or a socket is returned.
+            var shouldLoop = true
+            while (shouldLoop) {
+                val socket: BluetoothSocket? = try {
+                    Log.d(TAG, "run: Starting to listen for connection...")
+                    mmServerSocket?.accept()
+                } catch (e: IOException) {
+                    Log.e(TAG, "Socket's accept() method failed", e)
+                    shouldLoop = false
+                    null
+                }
+                socket?.also {
+                    btSocket = it
+                    if (btSocket?.isConnected == true)
+                        Toast.makeText(applicationContext, "Socket Created", Toast.LENGTH_SHORT).show()
+                    else Toast.makeText(applicationContext, "Socket Failed", Toast.LENGTH_SHORT).show()
+                    mmServerSocket?.close()
+                    shouldLoop = false
+                }
+            }
+        }
+
+        // Closes the connect socket and causes the thread to finish.
+        fun cancel() {
+            try {
+                mmServerSocket?.close()
+            } catch (e: IOException) {
+                Log.e(TAG, "Could not close the connect socket", e)
+            }
+        }
+    }*/
+
+
+    /*private fun connectToBtDevice() {
         val btAdapter = BluetoothAdapter.getDefaultAdapter()
         val remoteDevice = btAdapter.getRemoteDevice("94:B5:55:2D:29:32")
 
@@ -83,7 +161,7 @@ class MainActivity : AppCompatActivity() {
         }else{
             //deny
         }
-    }
+    }*/
 
     private fun sendDataToBT(signal: String) {
         val ssidOutputStream: OutputStream = btSocket!!.outputStream
